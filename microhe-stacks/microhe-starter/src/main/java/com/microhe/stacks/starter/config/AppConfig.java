d636a6e2fb754e5f84f4b229664d3b96c5fd8e41
zhangchunhui <chunhui2001@gmail.com>
2020-08-05 03:39:41 +0800

    a
    a

--- microhe-stacks/microhe-starter/src/main/java/com/microhe/stacks/starter/config/AppConfig.java
+++ microhe-stacks/microhe-starter/src/main/java/com/microhe/stacks/starter/config/AppConfig.java
@@ -1,6 +1,5 @@
 package com.microhe.stacks.starter.config;
 
-import java.util.List;
 import org.apache.logging.log4j.ThreadContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,16 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringBootVersion;
 import org.springframework.boot.builder.ParentContextApplicationContextInitializer.ParentContextAvailableEvent;
-import org.springframework.boot.context.event.ApplicationEnvironmentPreparedEvent;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.boot.env.EnvironmentPostProcessor;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.AnnotationAwareOrderComparator;
 import org.springframework.core.env.Environment;
-import org.springframework.core.io.support.SpringFactoriesLoader;
 
 @Configuration
 public class AppConfig implements ApplicationListener<ApplicationEvent> {
